7ba6aa2c9c868f3d65b5ff4480e36cc548b75bfa
JonTabiendo <jontabiendo@gmail.com>
2023-07-12 11:24:50 -0400

    session.js fix2

--- react-app/src/components/SignupFormPage/index.js
+++ react-app/src/components/SignupFormPage/index.js
@@ -19,7 +19,7 @@ function SignupFormPage() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (password === confirmPassword) {
-        const data = await dispatch(signUp(username, email, password));
+        const data = await dispatch(signUp(firstName, lastName, email, password));
         if (data) {
           setErrors(data)
         }
