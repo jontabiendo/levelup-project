50c2cc53938ee6d436fcab7a679666ec7334c6c7
JonTabiendo <jontabiendo@gmail.com>
2023-07-20 18:25:21 -0400

    background changed

--- react-app/src/components/Background/index.js
+++ react-app/src/components/Background/index.js
@@ -2,12 +2,15 @@ import React, { useRef, useEffect } from 'react'
 
 import './background.css'
 
+import video from "../../assets/compressed-video.mov"
+
 const Background = () => {
     return (
         <>
             <div id='video-overlay' onClick={null}></div>
             <div id='background'>
-            <iframe src="https://www.youtube.com/embed/4jxeYfqevl4?autoplay=1&muted=0&loop=1" title="YouTube video player" frameBorder="0" allow="autoplay" allowFullscreen></iframe>
+            {/* <iframe src="https://www.youtube.com/embed/4jxeYfqevl4?autoplay=1&muted=0&loop=1" title="YouTube video player" frameBorder="0" allow="autoplay" allowFullscreen></iframe> */}
+            <video src={video} autoPlay muted loop />
             </div>
         </>
     )
